5ef16fcf9e73604dcb0db760ad297bf27d723937
윤 대훈 <ketaro01@jinhaksa.net>
2018-11-30 15:54:11 +0900

    format 추가

--- src/page/Mapping.js
+++ src/page/Mapping.js
@@ -225,9 +225,10 @@ class Mapping extends Component {
             <Table>
               <colgroup>
                 <col width="20%" />
+                <col width="15%" />
+                <col width="15%" />
                 <col width="20%" />
-                <col width="25%" />
-                <col width="25%" />
+                <col width="20%" />
                 <col width="10%" />
               </colgroup>
               <TableHead>
